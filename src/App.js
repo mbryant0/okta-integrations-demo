f83dca075e6bb91ccee812f8e4b9755950fec638
Mikayla Bryant <mikaylabryant25@gmail.com>
2021-06-15 17:18:03 -0400

    Added carousel UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { Carousel } from 'react-responsive-carousel';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Carousel className='carousel-container'>
+        <div className='card'>
+          <p>OIDC (OpenID Connect) Integration</p>
+        </div>
+        <div className='card'>
+          <p>SAML Integration</p>
+        </div>
+        <div className='card'>
+          <p>SCIM Integration</p>
+        </div>
+        <div className='card'>
+          <p>SWA Integration</p>
+        </div>
+      </Carousel>
     </div>
   );
 }

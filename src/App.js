fdc99d43152885879ccb35ecbf6de3f92548b8bd
Mikayla Bryant <mikaylabryant25@gmail.com>
2021-06-15 18:08:49 -0400

    Added sign in forms

--- src/App.js
+++ src/App.js
@@ -1,21 +1,69 @@
 import './App.css';
 import { Carousel } from 'react-responsive-carousel';
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className='App'>
       <Carousel className='carousel-container'>
         <div className='card'>
-          <p>OIDC (OpenID Connect) Integration</p>
+          <div className='signin-card'>
+            <p>OIDC (OpenID Connect) Integration</p>
+            <Form>
+              <Form.Group>
+                <Form.Label>Email</Form.Label>
+                <Form.Control name='email' type='text' autoComplete='off' />
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  name='password'
+                  type='password'
+                  autoComplete='off'
+                />
+              </Form.Group>
+            </Form>
+          </div>
         </div>
         <div className='card'>
-          <p>SAML Integration</p>
+          <div className='signin-card'>
+            <p>SAML Integration</p>
+            <Form>
+              <Form.Group>
+                <Form.Label>Email</Form.Label>
+                <Form.Control name='email' type='text' autoComplete='off' />
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  name='password'
+                  type='password'
+                  autoComplete='off'
+                />
+              </Form.Group>
+            </Form>
+          </div>
         </div>
         <div className='card'>
-          <p>SCIM Integration</p>
-        </div>
-        <div className='card'>
-          <p>SWA Integration</p>
+          <div className='signin-card'>
+            <p>SWA Integration</p>
+            <Form>
+              <Form.Group>
+                <Form.Label>Email</Form.Label>
+                <Form.Control name='email' type='text' autoComplete='off' />
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  name='password'
+                  type='password'
+                  autoComplete='off'
+                />
+              </Form.Group>
+            </Form>
+          </div>
         </div>
       </Carousel>
     </div>
